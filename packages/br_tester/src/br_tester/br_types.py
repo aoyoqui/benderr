afc7dfc32852664531a2cd77d4df6e05d633bc73
Abraham Oyoqui <oyoqui@gmail.com>
2025-05-18 13:42:05 +0200

    Add packages folder, and tester types

---
+++ packages/br_tester/src/br_tester/br_types.py
@@ -0,0 +1,75 @@
+from typing import Any
+from dataclasses import dataclass
+
+from enum import StrEnum
+
+class NoSpecAction(StrEnum):
+    LOG = "log"
+    IGNORE = "ignore"
+
+class NumericComparator(StrEnum):
+    GT = ">"
+    GE = ">="
+    LT = "<"
+    LE = "<="
+    EQ = "=="
+    GTLT = "> <"
+    GELT = ">= <"
+    GTLE = "> <="
+    GELE = ">= <="
+    LTGT = "< >"
+    LTGE = "< >="
+    LEGT = "<= >"
+    LEGE = "<= >="
+
+@dataclass
+class NoSpec:
+    action: NoSpecAction
+
+@dataclass
+class NumericSpec:
+    comparator: NumericComparator
+    lower: float | None = None
+    upper: float | None = None
+    units: str = ""
+
+@dataclass
+class StringSpec:
+    expected_value: str
+    case_sensitive: bool
+
+@dataclass
+class Spec:
+    name: str
+    spec: NoSpec | NumericSpec | StringSpec
+
+@dataclass
+class Measurement:
+    value: Any
+    passed: bool
+    spec: Spec
+
+@dataclass
+class Step:
+    id: int
+    name: str
+    specs: list[Spec] = None
+
+@dataclass
+class StepResult:
+    id: int
+    name: str
+    results: list[Measurement] = None
+
+@dataclass
+class Sequence:
+    steps: list[Step] = None
+
+    def __len__(self):
+        if self.steps:
+            return len(self.steps)
+        else:
+            return 0
+        
+    def __getitem__(self, index):
+        return self.steps[index]
