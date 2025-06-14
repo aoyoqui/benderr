9fe64b25cc678547c628f5390a017e876115fc3e
Abraham Oyoqui <oyoqui@gmail.com>
2025-06-14 09:09:54 +0200

    Fix step names for demo sequence

--- packages/demos/src/br_demos/demo_sequence.py
+++ packages/demos/src/br_demos/demo_sequence.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 from br_tester.configurator import steps_from_file
@@ -6,21 +7,23 @@ from br_tester.sequence import Sequence
 
 class DemoSequence(Sequence):
     def sequence(self):
-        self.step(self.step_1)
-        self.step(self.step_2)
-        self.step(self.step_3)
-        self.step(lambda: 1)
-        self.step(lambda: -1)
-        self.step(lambda: 0)
-        self.step(lambda: 0.5)
+        self.step(self.step_1, step_name="Step 1. Boolean Spec")
+        self.step(self.step_2, step_name="Step 2. Boolean Spec")
+        self.step(self.step_3, step_name="Step 3. No Specs")
+        self.step(lambda: 1, step_name="Step 4. Lower limit")
+        self.step(lambda: -1, step_name="Step 5. Upper limit")
+        self.step(lambda: 0, step_name="Step 6. Equality (use lower limit)")
+        self.step(lambda: 0.5, step_name="Step 7. Lower and upper limit")
 
     def step_1(self):
         return True
 
     def step_2(self):
-        return False
+        time.sleep(1)
+        return True
 
     def step_3(self):
+        time.sleep(1)
         pass
 
 
